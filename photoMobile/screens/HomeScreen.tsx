420ef3a1af523942844957af1457f139cce14da5
ricardozv <ricardosilvanet03@gmail.com>
2022-10-08 12:45:30 -0300

    insert image on HomeScreen

--- photoMobile/screens/HomeScreen.tsx
+++ photoMobile/screens/HomeScreen.tsx
@@ -1,11 +1,16 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home Screen</Text>
+      <View style={ styles.smallContainer}>
+      <Text style={styles.title}>Not </Text>
+      < Image  source = {{ uri:'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg'}}
+      style = { styles.Image }
+      />
+      </View>
     </View>
   );
 }
@@ -15,14 +20,19 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    margin: 10
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  Image: {
+    width: '100%',
+    height: 200, 
+    borderRadius: 23
   },
+  smallContainer: {
+    width: '100%'
+  }
 });
